2ac8c6b4582b9e6d9f52e6535bb7830cb38de9a8
moez-rd <slow9ie@gmail.com>
2024-03-05 22:09:40 +0700

    init

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -31,7 +31,7 @@ def create_by_last_month_df(df):
     return last_month_df
 
 
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("./dashboard/all_data.csv")
 
 all_df["date"] = pd.to_datetime(all_df["date"])
 
